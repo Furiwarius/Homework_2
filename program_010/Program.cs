61485a02914b49ef33a40cbafa23de5d1886c31f
Vasiliy <vasiliy.trunov.00@mail.ru>
2022-08-13 13:35:21 +0200

    Задача 10

---
+++ program_010/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//1 способ
+string random = new Random().Next(100,1000).ToString();
+
+System.Console.WriteLine($"Трехзначное число №1 - {random}");
+System.Console.WriteLine($"Его вторая цифра - {random[1]}");
+System.Console.WriteLine("-------------------------------");
+
+//2 способ
+int random_1 = new Random().Next(100,1000);
+
+System.Console.WriteLine($"Трехзначное число №2 - {random_1}");
+if ((random_1/10)%10==0) System.Console.WriteLine("0");
+else System.Console.WriteLine($"Его вторая цифра - {(random_1/10)%10}");
+
